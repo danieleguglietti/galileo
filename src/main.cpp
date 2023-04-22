9e61f865affbe672f39dab1c8826aa4081a16535
Daniele Guglietti <gugliettidaniele@gmail.com>
2023-04-22 09:47:52 +0200

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <raylib.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    InitWindow(1280, 720, "Galileo");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing(); {
+            ClearBackground(Color { 110, 37, 206, 255 });
+            DrawText("skaman insegna Vulkan", 550, 360, 16, Color { 255, 255, 255, 255 });
+        } EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
